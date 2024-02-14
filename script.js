aa3013c2f02eee94fe1f06e61d6d49619d56132f
sputnikuno <sputnikuno@outlook.com>
2024-02-14 00:13:11 -0300

    desencriptado listo

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let botonEncriptar = document.getElementById("encriptar");
+let botonDesencriptar = document.getElementById("desencriptar");
 let textoProcesado = document.getElementById("textoProcesado");
 let imgLateral = document.getElementById("img-lateral");
 
@@ -10,11 +11,12 @@ function encriptar() {
     textoAEncriptar = textoAEncriptar.replaceAll("a", "ai");
     textoAEncriptar = textoAEncriptar.replaceAll("o", "ober");
     textoAEncriptar = textoAEncriptar.replaceAll("u", "ufat");
-    
+
+    //mostrar texto procesado
     textoProcesado.value = textoAEncriptar;
     imgLateral.style.display = "none";
     textoProcesado.style.display = "block";
-    console.log(textoAEncriptar);
+    //console.log(textoAEncriptar);
     /*"e" = "enter";
     "i" = "imes";
     "a" = "ai";
@@ -23,7 +25,24 @@ function encriptar() {
 
 }
 
+function desencriptar() {
+    let textoDesencriptar = document.getElementById("textoEncriptar").value;
+    
+    textoDesencriptar = textoDesencriptar.replaceAll("enter", "e");
+    textoDesencriptar = textoDesencriptar.replaceAll("imes", "i");
+    textoDesencriptar = textoDesencriptar.replaceAll("ai", "a");
+    textoDesencriptar = textoDesencriptar.replaceAll("ober", "o");
+    textoDesencriptar = textoDesencriptar.replaceAll("ufat", "u");
+
+    //mostrar texto procesado
+    textoProcesado.value = textoDesencriptar;
+    imgLateral.style.display = "none";
+    textoProcesado.style.display = "block";
+
+    //console.log(textoDesencriptar);
+}
+
+botonDesencriptar.onclick = desencriptar;
 botonEncriptar.onclick = encriptar;
 
 
-//mostrar texto procesado
