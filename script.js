78cd65045fd0f51532d9dba488b99ecb0a3688b8
sputnikuno <sputnikuno@outlook.com>
2024-02-14 00:00:43 -0300

    encriptado listo

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-function encriptar() {
-    let texto = document.getElementById("textoEncriptar").value;
-    console.log(texto);
-}
-
-function desencriptar() {
-
-}
-
-function copiar() {
-
-}
-
 let botonEncriptar = document.getElementById("encriptar");
+let textoProcesado = document.getElementById("textoProcesado");
+let imgLateral = document.getElementById("img-lateral");
+
+function encriptar() {
+    let textoAEncriptar = document.getElementById("textoEncriptar").value;
+
+    textoAEncriptar = textoAEncriptar.replaceAll("e", "enter");
+    textoAEncriptar = textoAEncriptar.replaceAll("i", "imes");
+    textoAEncriptar = textoAEncriptar.replaceAll("a", "ai");
+    textoAEncriptar = textoAEncriptar.replaceAll("o", "ober");
+    textoAEncriptar = textoAEncriptar.replaceAll("u", "ufat");
+    
+    textoProcesado.value = textoAEncriptar;
+    imgLateral.style.display = "none";
+    textoProcesado.style.display = "block";
+    console.log(textoAEncriptar);
+    /*"e" = "enter";
+    "i" = "imes";
+    "a" = "ai";
+    "o" = "ober";
+    "u" = "ufat";*/
+
+}
+
 botonEncriptar.onclick = encriptar;
 
-texto.setSelectionRange(0, 0);
 
-let botonDesencriptar = document.getElementById("desencriptar");
-
-let botonCopiar = document.getElementById("boton-copiar");
-botonCopiar.onclick = copiar;
+//mostrar texto procesado
